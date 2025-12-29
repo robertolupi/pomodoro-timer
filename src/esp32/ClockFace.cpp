1c427ebe435fbbce071e944ce0be678b41fbae9b
Roberto Lupi <roberto.lupi@gmail.com>
2025-12-29 10:49:17 +0100

    ClockFace: include weekday.

--- src/esp32/ClockFace.cpp
+++ src/esp32/ClockFace.cpp
@@ -4,19 +4,31 @@
 
 #include "ClockFace.h"
 
+const char* days_of_week[] = {
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+};
+
+int weekday(const time_t t)
+{
+    struct tm *timeinfo = localtime(&t);
+    return timeinfo->tm_wday;
+}
+
 inline void ClockFace::notification(ClockUpdate update)
 {
   char time_buffer[sizeof("HH:MM:SS")];
   char time_buffer2[sizeof("DD MM YYYY")];
   strftime(time_buffer, sizeof(time_buffer), "%H:%M:%S", localtime(&update.now));
   strftime(time_buffer2, sizeof(time_buffer2), "%d %m %Y", localtime(&update.now));
+  int weekday_index = weekday(update.now) % 7;
   char remaining_time_buffer[sizeof("MM:SS")];
   strftime(remaining_time_buffer, sizeof(remaining_time_buffer), "%M:%S", gmtime(&update.remaining_time_in_state));
   switch(update.state) {
   case IDLE:
     canvas_.fillScreen(BLACK);
     drawTime(time_buffer, WHITE, 0, 7, 1);
-    drawTime(time_buffer2, WHITE, 1, 6, 1);
+    drawTime(time_buffer2, YELLOW, 1, 4, 2);
+    drawTime(days_of_week[weekday_index], YELLOW, 2, 4, 2);
     break;
   case WORK:
     canvas_.fillScreen(DARKGREEN);
